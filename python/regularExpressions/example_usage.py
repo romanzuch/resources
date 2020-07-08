c8eec15f878e463aa49b44e1439b43553cbfeb79
romanzuch <42408195+romanzuch@users.noreply.github.com>
2020-07-08 14:56:43 +0200

    Update example_usage.py

--- python/regularExpressions/example_usage.py
+++ python/regularExpressions/example_usage.py
@@ -2,17 +2,15 @@
 
 import re
 
-# import the given text
-searchableText = open(example_text.txt)
-
 # define patter to search for Lorem in given text
 searchPattern = re.compile("lorem")
 
 # iterate through the lines of the text 
 findings: Int = 0
-for line in searchableText:
-  if searchPattern.fullmatch(line.lower()):
-    findings += 1
+with open(example_text.txt) as searchableText:
+  for line in searchableText:
+    if searchPattern.fullmatch(line.lower()):
+      findings += 1
 
 print("Found: {}".format(findings))
   
