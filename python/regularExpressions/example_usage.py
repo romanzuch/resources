5094d8eab5896e4295b57eb011fa221ed2295b76
romanzuch <42408195+romanzuch@users.noreply.github.com>
2020-07-08 14:53:46 +0200

    Update example_usage.py

--- python/regularExpressions/example_usage.py
+++ python/regularExpressions/example_usage.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python python3 
 
 import re
+
+# import the given text
+searchableText = open(example_text.txt)
+
+# define patter to search for Lorem in given text
+searchPattern = re.compile("lorem")
+
+# iterate through the lines of the text 
+findings: Int = 0
+for line in searchableText:
+  if searchPattern.fullmatch(line.lower()):
+    findings += 1
+
+print("Found: {}".format(findings))
+  
